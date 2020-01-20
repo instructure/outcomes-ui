4d120d1c379c2e642bf3ad90b1bed58c3a22f974
Michael Brewer-Davis <mbrewerdavis@instructure.com>
2020-01-20 14:41:57 -0600

    fix bundle pre-translation
    - also removes dependency on quiz-presets
    
    Test plan:
    - run
        yarn run prepublishOnly
    - verify that files in lib/translated have
      been pretranslated
      (e.g., lib/translated/fr/components/AlignmentList/index.js
      should have a SR notification with
        alignement supprim\xE9
      instead of
        alignment removed
    
    Change-Id: Ieaaf49bfe05cae2282b4719aa23f813ffbf32d1a
    Reviewed-on: https://gerrit.instructure.com/c/outcomes-ui/+/223878
    Reviewed-by: Pat Renner <prenner@instructure.com>
    Reviewed-by: Augusto Callejas <acallejas@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Michael Brewer-Davis <mbd@instructure.com>
    QA-Review: Michael Brewer-Davis <mbd@instructure.com>

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,37 @@
+const path = require('path')
+
 // eslint-disable-next-line immutable/no-mutation
 module.exports = {
   presets: [
-    [
-      'quiz-presets/babel-preset-instructure-quizzes',
-      {
-        translationsDir: './translations',
-        extractDefaultTranslations: false
-      }
-    ],
     ['@instructure/ui-babel-preset', {
       themeable: !process.env.DEBUG && process.env.NODE_ENV !== 'test',
       coverage: process.env.NODE_ENV === 'test'
     }]
   ],
   plugins: [
-    "inline-react-svg"
+    "inline-react-svg",
+    ...getFormatMessageConfig()
   ]
+}
+
+function getFormatMessageConfig () {
+  const translationsDir = './translations'
+
+  let formatMessageConfig = []
+  // if a BUILD_LOCALE environment variable is set, generate pre-translated source for that language,
+  const BUILD_LOCALE = process.env.BUILD_LOCALE
+  if (BUILD_LOCALE) {
+    formatMessageConfig = [
+      ['transform-format-message', {
+        generateId: 'underscored_crc32',
+        inline: true,
+        locale: BUILD_LOCALE,
+        translations: {
+          // eslint-disable-next-line security/detect-non-literal-require
+          [BUILD_LOCALE]: require(path.join(process.cwd(), translationsDir, BUILD_LOCALE))
+        }
+      }]
+    ]
+  }
+  return formatMessageConfig
 }
