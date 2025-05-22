46a1ab1ccd2b54bca14e7adb7b592a88c5747ce0
balazs.buri <balazs.buri@instructure.com>
2025-05-22 10:17:19 +0200

    refactor tests
    refs: EGG-1256
    
    Test Plan:
    - Automated tests PASS
    
    Change-Id: Ic085fc52cc4565869092c7f6d479492e697bf8ab
    Reviewed-on: https://gerrit.instructure.com/c/outcomes-ui/+/379076
    Reviewed-by: Jason Gillett <jason.gillett@instructure.com>
    QA-Review: Peipei Zhou <peipei.zhou@instructure.com>
    Product-Review: Kyle Rosenbaum <krosenbaum@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- src/store/__tests_jest_rtl__/index.test.js
+++ src/store/__tests_jest_rtl__/index.test.js
@@ -1,5 +1,4 @@
-import { expect } from 'chai'
-import sinon from 'sinon'
+import { expect, jest, describe, it } from '@jest/globals'
 import { getStore, makeScope, makeUserScope } from '../index'
 
 const host = 'http://outcomes.docker'
@@ -7,43 +6,52 @@ const jwt = 'jwt'
 
 describe('getStore', () => {
   it('exports a function', () => {
-    expect(getStore).to.be.a('function')
+    expect(typeof getStore).toBe('function')
   })
 
   it('returns an object', () => {
     const store = getStore(host, jwt, 'scope')
-    expect(store).to.be.an('object')
+    expect(typeof store).toBe('object')
   })
 
   it('returns a well formed store', () => {
     const store = getStore(host, jwt, 'scope')
-    expect(store).to.contain.all.keys(
-      'dispatch subscribe getState replaceReducer'.split(' ')
-    )
+    expect(store).toHaveProperty('dispatch')
+    expect(store).toHaveProperty('subscribe')
+    expect(store).toHaveProperty('getState')
+    expect(store).toHaveProperty('replaceReducer')
   })
 
   it('returns the same store for multiple scopes', () => {
     const store = getStore(host, jwt, 'scope')
     const store2 = getStore(host, jwt, 'scope2')
-    expect(store2).to.equal(store)
+    expect(store2).toBe(store)
   })
 
   it('creates state for each scope', () => {
     const store = getStore(host, jwt, 'scope')
     getStore(host, jwt, 'scope2')
-    expect(store.getState().toJS()).to.include.keys('scope', 'scope2')
+
+    // Using toJS() to convert Immutable data structure to plain JS
+    const storeState = store.getState().toJS()
+    expect(storeState).toHaveProperty('scope')
+    expect(storeState).toHaveProperty('scope2')
   })
 
   it('calls replaceReducer per unique scope', () => {
     const store = getStore(host, jwt, 'scope')
-    const stub = sinon.stub(store, 'replaceReducer')
+
+    // Use Jest's spyOn instead of sinon.stub
+    jest.spyOn(store, 'replaceReducer')
+
     getStore(host, jwt, 'scope')
     getStore(host, jwt, 'scopefoo')
     getStore(host, jwt, 'scopefoo')
     getStore(host, jwt, 'scopebar')
     getStore(host, jwt, 'scopebar')
-    // replaceReducer already called once for 'scope' before we stub
-    expect(stub).to.have.been.calledTwice
+
+    // replaceReducer already called once for 'scope' before we spy
+    expect(store.replaceReducer).toHaveBeenCalledTimes(2)
   })
 })
 
@@ -51,13 +59,13 @@ describe('makeScope', () => {
   it('returns unique scope string for different ids', () => {
     const thing1 = makeScope('thing', 1)
     const thing2 = makeScope('thing', 2)
-    expect(thing1).not.to.equal(thing2)
+    expect(thing1).not.toBe(thing2)
   })
 
   it('returns unique scope for different types', () => {
     const foo = makeScope('foo', 1)
     const bar = makeScope('bar', 1)
-    expect(foo).not.to.equal(bar)
+    expect(foo).not.toBe(bar)
   })
 })
 
@@ -65,6 +73,6 @@ describe('makeUserScope', () => {
   it('returns unique user scope string for different user uuids', () => {
     const thing1 = makeUserScope('user1')
     const thing2 = makeUserScope('user2')
-    expect(thing1).not.to.equal(thing2)
+    expect(thing1).not.toBe(thing2)
   })
 })
